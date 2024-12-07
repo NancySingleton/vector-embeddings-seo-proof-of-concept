f816867fbe08881ad4e7a25593865468f767a36d
Nancy Singleton <nancysingleton_@hotmail.co.uk>
2024-12-07 20:51:24 +0000

    Move csv reading logic out of matching_helper

--- use-case-1/step-3/find_matches.py
+++ use-case-1/step-3/find_matches.py
@@ -1,3 +1,4 @@
+import ast
 from utils.csv_helper import read, write
 from utils.matching_helper import get_best_match
 
@@ -7,8 +8,13 @@ KEYWORDS_EMBEDDINGS_FILE = PREFIX + 'step_2_output.csv'
 PAGE_MATCH_BY_KEYWORD_FILE = PREFIX + 'best_page_match_by_keyword.csv'
 KEYWORD_MATCH_BY_PAGE_FILE = PREFIX + 'best_keyword_match_by_page.csv'
 
-page_embeddings = read(PAGE_EMBEDDINGS_FILE)
-keyword_embeddings = read(KEYWORDS_EMBEDDINGS_FILE)
+
+def read_embeddings(filename):
+    return [[e[0], ast.literal_eval(e[1])] for e in read(filename)]
+
+
+page_embeddings = read_embeddings(PAGE_EMBEDDINGS_FILE)
+keyword_embeddings = read_embeddings(KEYWORDS_EMBEDDINGS_FILE)
 
 page_matches_by_keyword = [get_best_match(keyword_embedding, page_embeddings) for keyword_embedding in keyword_embeddings]
 write(PAGE_MATCH_BY_KEYWORD_FILE, page_matches_by_keyword)
--- utils/matching_helper.py
+++ utils/matching_helper.py
@@ -1,18 +1,13 @@
-import ast
 import annoy
 
 VECTOR_LENGTH = 1536
-
-
-def get_vector(embedding):
-    return ast.literal_eval(embedding[1])
 
 
 def build_index(embeddings):
     index = annoy.AnnoyIndex(VECTOR_LENGTH, 'dot')
 
     for idx, embedding in enumerate(embeddings):
-        index.add_item(idx, get_vector(embedding))
+        index.add_item(idx, embedding[1])
 
     index.build(10)
     return index
@@ -20,7 +15,7 @@ def build_index(embeddings):
 
 def get_best_match(embedding, potential_matches):
     item_name = embedding[0]
-    item_vector = get_vector(embedding)
+    item_vector = embedding[1]
 
     index_of_potential_matches = build_index(potential_matches)
 
