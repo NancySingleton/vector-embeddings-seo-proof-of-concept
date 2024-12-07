3e6aa2da2d5c182f759b6c8c530c29ee8f1e5400
Nancy Singleton <nancysingleton_@hotmail.co.uk>
2024-12-07 21:22:24 +0000

    Change example data

--- use-case-2/find_matches.py
+++ use-case-2/find_matches.py
@@ -15,5 +15,5 @@ def get_embeddings_from_csv(filename):
 embeddings_for_required_items = get_embeddings_from_csv(REQUIRED_FILE)
 embeddings_for_existing_items = get_embeddings_from_csv(EXISTING_FILE)
 
-best_existing_match_for_each_required_item = [get_best_matches(req, embeddings_for_existing_items, 5) for req in embeddings_for_required_items]
+best_existing_match_for_each_required_item = [get_best_matches(req, embeddings_for_existing_items, 3) for req in embeddings_for_required_items]
 write(OUTPUT_FILE, best_existing_match_for_each_required_item)
--- utils/csv_helper.py
+++ utils/csv_helper.py
@@ -9,5 +9,5 @@ def read(filename):
 
 def write(filename, content):
     with open(filename, 'w') as output_file:
-        writer = csv.writer(output_file, delimiter='|', quoting=csv.QUOTE_NONE)
+        writer = csv.writer(output_file, delimiter='|', quoting=csv.QUOTE_NONE, escapechar='\\')
         writer.writerows(content)
